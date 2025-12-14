11719a86f05198d33c8d5c9e70b15722088a259c
Alexis Nardiello <alexisnardiello@gmail.com>
2025-12-14 15:09:59 +0100

    Small changes

--- src/app/media/_components/Player.tsx
+++ src/app/media/_components/Player.tsx
@@ -27,12 +27,6 @@ const Player = () => {
 	useSocket<State>("video-seek", state => {
 		if (ref.current) ref.current.currentTime = state.time;
 	});
-	useSocket<State>("shorts-next", state => {
-		setId(state.id);
-	});
-	useSocket<State>("shorts-prev", state => {
-		setId(state.id);
-	});
 
 	useEffect(() => {
 		setIsMounted(true);
@@ -49,7 +43,7 @@ const Player = () => {
 				playing={!paused}
 				width="100%"
 				height="100%"
-				controls={false}
+				loop
 			/>
 		</section>,
 		document.body
