cfb6cc67d76586a26c2054b72c7d93250d888f99
Alexis Nardiello <alexisnardiello@gmail.com>
2026-07-14 16:23:00 +0200

    Small changes

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,6 +1,6 @@
 const Media = () => (
 	<section className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center bg-black z-50">
-		<p className="text-lg text-white/60">No room specified.</p>
+		<p className="text-lg text-white/60">No room specified</p>
 	</section>
 );
 
